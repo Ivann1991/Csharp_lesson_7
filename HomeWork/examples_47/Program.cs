268adb120e6d07cc7843b6bfa98dec15fb7e1662
Ivan <Korneychik91@yandex.ru>
2022-10-25 22:21:33 +0300

    examples_47, 48 done

---
+++ HomeWork/examples_47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Clear();
+Console.Write("Введи количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введи количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+double [,] array = getArray(rows, columns, -10, 10);
+printArray (array);
+
+
+
+
+
+
+//////////////////////////////////////////////
+double [,] getArray(int m, int n, int minValue, int maxValue)        //////////заполнение двухмерного массива случайными числами.
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue) + Math.Round(new Random().NextDouble(), 2);
+        }
+    }
+    return array;
+}
+
+void printArray (double [,] array)                                    ///////////вывод двухмерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array [i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ HomeWork/examples_50/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Console.Clear();
+int rows = new Random().Next(1, 11);
+int columns = new Random().Next(1, 11);
+int[,] array = getArray(rows, columns);
+printArray(array);
+Console.Write($"Введи номер строки элемента массива от 1 до {rows}: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.Write($"Введи номер столбца элемента массива от 1 до {columns}: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+if (a < 1)
+    Console.WriteLine("Строки с таким номером не существует");
+else if (a > array.GetLength(0))
+        Console.WriteLine("Строки с таким номером не существует");
+else if (b < 1)
+        Console.WriteLine("Столбца с таким номером не существует");
+else if (b > array.GetLength(1))
+        Console.WriteLine("Столбца с таким номером не существует");
+else                          
+Console.WriteLine(array[a-1, b-1]);
+
+
+//////////////////////////////////////////////
+int[,] getArray(int m, int n)        //////////заполнение двухмерного массива случайными числами.
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+    return array;
+}
+
+
+void printArray(int[,] array)                                    ///////////вывод двухмерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
