4b9b123562c81e8d46c0e4f0a1f6fedc8d9f220c
Ivan <Korneychik91@yandex.ru>
2022-10-26 22:31:05 +0300

    examples_52 done

--- HomeWork/examples_50/Program.cs
+++ HomeWork/examples_50/Program.cs
@@ -1,26 +1,4 @@
 ﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-Console.Clear();
-int rows = new Random().Next(1, 11);
-int columns = new Random().Next(1, 11);
-int[,] array = getArray(rows, columns);
-printArray(array);
-Console.Write($"Введи номер строки элемента массива от 1 до {rows}: ");
-int a = int.Parse(Console.ReadLine() ?? "");
-Console.Write($"Введи номер столбца элемента массива от 1 до {columns}: ");
-int b = int.Parse(Console.ReadLine() ?? "");
-if (a < 1)
-    Console.WriteLine("Строки с таким номером не существует");
-else if (a > array.GetLength(0))
-        Console.WriteLine("Строки с таким номером не существует");
-else if (b < 1)
-        Console.WriteLine("Столбца с таким номером не существует");
-else if (b > array.GetLength(1))
-        Console.WriteLine("Столбца с таким номером не существует");
-else                          
-Console.WriteLine(array[a-1, b-1]);
-
-
-//////////////////////////////////////////////
 int[,] getArray(int m, int n)        //////////заполнение двухмерного массива случайными числами.
 {
     int[,] array = new int[m, n];
@@ -33,8 +11,6 @@ int[,] getArray(int m, int n)        //////////заполнение двухме
     }
     return array;
 }
-
-
 void printArray(int[,] array)                                    ///////////вывод двухмерного массива
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -46,3 +22,31 @@ void printArray(int[,] array)                                    ///////////вы
         Console.WriteLine();
     }
 }
+bool proverka(int a, int b, int[,] array)                         ///////////проверка на правилность ввода позиции элемента массива
+{
+    if (a < 1)
+        return true;
+    else if (a > array.GetLength(0))
+        return true;
+    else if (b < 1)
+        return true;
+    else if (b > array.GetLength(1))
+        return true;
+    else
+        return false;    
+}
+
+Console.Clear();
+int rows = new Random().Next(1, 11);
+int columns = new Random().Next(1, 11);
+int[,] array = getArray(rows, columns);
+printArray(array);
+Console.Write($"Введи номер строки элемента массива от 1 до {rows}: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.Write($"Введи номер столбца элемента массива от 1 до {columns}: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+bool result = proverka(a, b, array);
+if (result)
+    Console.WriteLine("Элемента с таким номером строки или столбца не существует");
+else 
+    Console.WriteLine($"Выбранный элемент массива это {array[a - 1, b - 1]}");            
---
+++ HomeWork/examples_52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] getArray(int m, int n, int minValue, int maxValue)        //////////заполнение двухмерного массива случайными числами.
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+void printArray(int[,] array)                                    ///////////вывод двухмерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void srArif(int[,] array)                                         ////// нахождение среднего арифметического элементов столбцов
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        int count = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            count += 1;
+        }
+        Console.Write($"\nСреднее арифметическое элементов {j + 1} столбца равно {Math.Round(sum / count, 2)} ");
+    }
+
+}
+Console.Clear();
+Console.Write("Введи количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введи количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = getArray(rows, columns, 0, 10);
+printArray(array);
+srArif(array);
